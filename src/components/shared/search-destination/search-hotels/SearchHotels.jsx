4c44f53afb9a0930f11faef1ee5c6ab3185f4eb7
Alberto Rodriguez <ticoc911@gmail.com>
2022-10-31 12:20:55 +0100

    proxy for cors

--- src/components/shared/search-destination/search-hotels/SearchHotels.jsx
+++ src/components/shared/search-destination/search-hotels/SearchHotels.jsx
@@ -4,7 +4,7 @@ import { BiBed } from 'react-icons/bi'
 import { ChoseInput } from '../shared/chose-input/ChoseInput'
 import { FaHotel } from 'react-icons/fa'
 import axios from 'axios'
-// import { xSignature } from '../../../../helpers/signature-generator'
+import { xSignature } from '../../../../helpers/signature-generator'
 import { useEffect } from 'react'
 const CryptoJS = require("crypto-js");
 
@@ -12,20 +12,6 @@ export const SearchHotels = () => {
 
   const [data, setData] = useState()
 
-  const publicKey = process.env.REACT_APP_API_KEY
-  const privateKey = process.env.REACT_APP_SECRET
-
-  const xSignature = () => {
-    // UTC creation
-    const utcDate = Math.floor(new Date().getTime() / 1000);
-
-    // Signature Assembly
-    const assemble = (publicKey + privateKey + utcDate);
-
-    // SHA-256 Encryption
-    const hash = CryptoJS.SHA256(assemble).toString()
-    return hash.toString(CryptoJS.enc.Hex)
-  }
 
     const other = () => {
 
@@ -33,13 +19,13 @@ export const SearchHotels = () => {
 
       var config = {
         method: 'get',
-        url: 'https://api.test.hotelbeds.com/hotel-api/1.0/status',
+        url: '/hotel-api/1.0/status',
         headers: {
           'Api-key': '691de985c93cbcfa7683a83f1f7ed7ae',
           'X-Signature': signature,
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
+  
         }
+      
       };
 
       axios(config)
