14c32a2da3e2463d9ad809c2540e13c72abbadf6
Alberto Rodriguez <ticoc911@gmail.com>
2022-11-17 00:45:25 +0100

    styling popover travelers

--- src/components/shared/search-destination/shared/travelers/Travelers.jsx
+++ src/components/shared/search-destination/shared/travelers/Travelers.jsx
@@ -4,6 +4,8 @@ import './styles.css'
 import { useState } from 'react'
 import { VscPerson } from 'react-icons/vsc'
 import { MdArrowDropDown } from 'react-icons/md'
+import { TiMinus } from 'react-icons/ti'
+import { GoPlus } from 'react-icons/go'
 
 export const Travelers = () => {
 
@@ -16,6 +18,50 @@ export const Travelers = () => {
     const textButtonTravelers = () => {
         const persons = travelers.adults + travelers.children
         return `${persons} travelers, ${travelers.rooms} room`
+    }
+
+    const handlePlusAdult = (e) => {
+        e.preventDefault()
+
+        if (travelers.adults < 10) {
+            setTravelers({
+                ...travelers,
+                adults: travelers.adults + 1
+            })
+        }
+    }
+
+    const handleMinusAdult = (e) => {
+        e.preventDefault()
+
+        if (travelers.adults > 1) {
+            setTravelers({
+                ...travelers,
+                adults: travelers.adults - 1
+            })
+        }
+    }
+
+    const handlePlusChild = (e) => {
+        e.preventDefault()
+
+        if (travelers.children < 10) {
+            setTravelers({
+                ...travelers,
+                children: travelers.children + 1
+            })
+        }
+    }
+
+    const handleMinusChild = (e) => {
+        e.preventDefault()
+
+        if (travelers.children > 0) {
+            setTravelers({
+                ...travelers,
+                children: travelers.children - 1
+            })
+        }
     }
 
     return (
@@ -45,26 +91,50 @@ export const Travelers = () => {
                     </span>
 
                     <div className='adults_set_container'>
+
                         <div className='info_text'>
                             <span className='description_adults'>Adults</span>
                             <span className='info_adults'>12+</span>
                         </div>
 
                         <div className='couter_container'>
-                            <button className='btn_add'>_</button>
+                            <button className='btn_add' onClick={handleMinusAdult}>
+                                <TiMinus />
+                            </button>
                             <span className='adults_cant'>
                                 {`${travelers.adults}`}
                             </span>
-                            <button className='btn_add'>+</button>
+                            <button className='btn_add' onClick={handlePlusAdult}>
+                                <GoPlus/>
+                            </button>
                         </div>
+
                     </div>
 
-                    <div>
-                        Kids
+                    <div className='adults_set_container'>
+
+                        <div className='info_text'>
+                            <span className='description_adults'>Childs</span>
+                            <span className='info_adults'>12-</span>
+                        </div>
+
+                        <div className='couter_container'>
+                            <button className='btn_add' onClick={handleMinusChild}>
+                                <TiMinus />
+                            </button>
+                            <span className='adults_cant'>
+                                {`${travelers.children}`}
+                            </span>
+                            <button className='btn_add' onClick={handlePlusChild}>
+                                <GoPlus />
+                            </button>
+                        </div>
+
                     </div>
+
                 </div>
 
-                <div>
+                <div className='btn_add_room_container'>
                     <button>Add room</button>
                 </div>
 
