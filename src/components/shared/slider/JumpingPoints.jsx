ebf906ebc45b1f7ccce9343f5b8b610049122a73
Alberto Rodriguez <ticoc911@gmail.com>
2022-12-04 13:50:35 +0100

    jumping componet done

--- src/components/shared/slider/JumpingPoints.jsx
+++ src/components/shared/slider/JumpingPoints.jsx
@@ -4,18 +4,30 @@ import './styles.css'
 
 export const JumpingPoints = ({ slides, currentIndex }) => {
 
-    
-  return (
-    <div className='points_container'>
-        <div className='dot_container'>
-              {
-                  slides.map((slide, index) => (
-                      <div 
-                        className={ index === currentIndex ? 'dot current' : 'dot' }
-                      ></div>
-                  ))
-              }
-        </div>   
-    </div>
-  )
+    const style = { transform: 'translate3d(' + (-currentIndex * 1.1) + 'rem, 0, 0)' }
+
+    const setOpacity = (index) => {
+        if (index < currentIndex - 2 || index > currentIndex + 2) return '0'
+
+        return ''
+    }
+
+    return (
+        <div className='points_container'>
+            <div className='overflow_hide_container'>
+                <div className='dot_container' style={style}>
+                    {
+                        slides.map((slide, index) => (
+                            <div
+                                className={index === currentIndex ? 'dot current' : 'dot'}
+                                style={{ opacity: setOpacity(index) }}
+                            ></div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
 }
+
+
