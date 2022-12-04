a4743b0f7ec8a25c38fad53fad7351ad7e84d61d
Alberto Rodriguez <ticoc911@gmail.com>
2022-12-04 02:18:26 +0100

    carousel working with touch

--- src/hooks/useTouch.js
+++ src/hooks/useTouch.js
@@ -4,8 +4,10 @@ import { MdNextPlan } from 'react-icons/md'
 
 export const useTouch = (slides) => {
 
-    const [touchPosition, setTouchPosition] = useState(null)
+
     const [currentIndex, setCurrentIndex] = useState(0)
+    const [touchStart, setTouchStart] = useState(null)
+    const [newPosition, setNewPosition] = useState(null)
     const [touch, setTouch] = useState({
         currentEvent: false,
         touchStartLocation: null,
@@ -13,53 +15,60 @@ export const useTouch = (slides) => {
     })
 
     // var screen = document.documentElement.clientWidth
-    
-    const basicStyle = { transform: 'translate3d(' + (-currentIndex * 100) + '%, 0, 0)' }
+
+    const basicStyle = {
+        transform: 'translate3d(' + (-currentIndex * 100) + '%, 0, 0)',
+    }
     const moveStyle = {
         transition: 'none',
-        transform: 'translate3d(' + (-touch.newPosition * 100) + '%, 0, 0)'
+        transform: 'translate3d(' + (-newPosition * 100) + '%, 0, 0)'
     }
 
     const handleTouchStart = (e) => {
-
-        const touchDown = e.touches[0].clientX
-        setTouchPosition(touchDown)
-
-        // setTouch({
-        //     ...touch,
-        //     touchStartLocation: e.touches[0].clientX
-        // })
+        setTouchStart(e.touches[0].clientX) 
     }
 
     const handleTouchMove = (e) => {
-        const touchDown = touchPosition
+        const lastPosition = e.touches[0].clientX
+        setNewPosition(getPosition(lastPosition))
 
-        if (touchDown === null) return
-
-        const currentTouch = e.touches[0].clientX 
-        const diff = touchDown - currentTouch
-
-        if (diff > 5) goToNext()
-
-        if (diff < -5) goToPrevious()
-
-        setTouchPosition(null)    
+        setTouch({
+            ...touch,
+            currentEvent: true,
+        })
     }
 
     const handleTouchEnd = (e) => {
+        const lastTouch = e.changedTouches[0].clientX
+        let index = Math.round(getPosition(lastTouch))
         
+        if (index < 0) {
+            index = 0
+        }else if (index > slides.length - 1) {
+            index = slides.length - 1
+        } 
+
+        setCurrentIndex(index)
+
+        setTouch({
+            ...touch,
+            currentEvent: false,
+        })
     }
 
-
+    const getPosition = (lastTouch) => {
+        const diff = touchStart - lastTouch
+        return currentIndex + diff / 330
+    }
 
     const goToPrevious = () => {
         const isFirstSlide = currentIndex === 0
         const newIndex = isFirstSlide ? currentIndex : currentIndex - 1
-        setCurrentIndex(newIndex) 
+        setCurrentIndex(newIndex)
         setTouch({
             ...touch,
-            newPosition: newIndex 
-        }) 
+            newPosition: newIndex
+        })
     }
 
     const goToNext = () => {
@@ -68,10 +77,10 @@ export const useTouch = (slides) => {
         setCurrentIndex(newIndex)
         setTouch({
             ...touch,
-            newPosition: newIndex 
+            newPosition: newIndex
         })
     }
-    
+
     const state = {
         onTouchStart: handleTouchStart,
         onTouchEnd: handleTouchEnd,
