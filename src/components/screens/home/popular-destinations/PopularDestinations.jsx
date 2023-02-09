e29fdf31b74d2b8e2461787b05b1a5de37e827ff
Alberto Rodriguez <ticoc911@gmail.com>
2023-02-09 02:40:00 +0100

    responsive cards destinations added

--- src/components/screens/home/popular-destinations/PopularDestinations.jsx
+++ src/components/screens/home/popular-destinations/PopularDestinations.jsx
@@ -9,33 +9,54 @@ import './styles.css'
 export const PopularDestinations = () => {
     return(
         <section className="popularDest-container">
-            <div className="container">
-                <div className="section-title text-center">
+            <div className="destination_area main_container">
+
+                <div className="titles_container">
                     <span className="section-tagline">Destination lists</span>
-                    <h2 className="section-title">Go Exotic Places</h2>
+                    <h2 className="section-title">Popular destinations in Cuba</h2>
                 </div>
-                <div className="section-PopularDestinations">
-                    <div className="destination-container">
+
+                <div className="zones_container first_zone">
+                    <div className="zone_destination-container">
                         <img src={habana} alt="hoteles en la habana" />
-                        <h3>La Habana</h3>
+                        {/* <div className="overlay_dest"></div> */}
+                        <div className="name_zone">
+                            <h3>La Habana</h3>
+                        </div>
                     </div>
-                    <div className="destination-container">
-                        <img src={santiago} alt="hoteles en la habana" />
-                        <h3>Santiago de Cuba</h3>
+                    <div className="zone_destination-container">
+                        <img src={varadero} alt="hoteles en varadero" />
+                        {/* <div className="overlay_dest"></div> */}
+                        <div className="name_zone">
+                            <h3>Varadero</h3>
+                        </div>
                     </div>
-                    <div className="destination-container">
-                        <img src={varadero} alt="hoteles en la habana" />
-                        <h3>Varadero</h3>
+                    <div className="zone_destination-container">
+                        <img src={guardalavaca} alt="hoteles en guardalavaca" />
+                        {/* <div className="overlay_dest"></div> */}
+                        <div className="name_zone">
+                            <h3>Guardalavaca</h3>
+                        </div>
+                    </div> 
+                </div>
+
+                <div className="zones_container second_zone">
+                    <div className="zone_destination-container">
+                        <img src={trinidad} alt="hoteles en trinidad" />
+                        {/* <div className="overlay_dest"></div> */}
+                        <div className="name_zone">
+                            <h3>Trinidad</h3>
+                        </div>
                     </div>
-                    <div className="destination-container">
-                        <img src={guardalavaca} alt="hoteles en la habana" />
-                        <h3>Guardalavaca</h3>
-                    </div>
-                    <div className="destination-container">
-                        <img src={trinidad} alt="hoteles en la habana" />
-                        <h3>Trinidad</h3>
+                    <div className="zone_destination-container">
+                        <img src={santiago} alt="hoteles en santiago de cuba" />
+                        {/* <div className="overlay_dest"></div> */}
+                        <div className="name_zone">
+                            <h3>Santiago de Cuba</h3>
+                        </div>
                     </div>
                 </div>
+
             </div >
         </section>
     )
