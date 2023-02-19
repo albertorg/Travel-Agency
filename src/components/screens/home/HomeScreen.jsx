e01db763365c047b0901bbac358c5404999d26dd
Alberto Rodriguez <ticoc911@gmail.com>
2023-02-19 05:12:52 +0100

    button styles done

--- src/components/screens/home/HomeScreen.jsx
+++ src/components/screens/home/HomeScreen.jsx
@@ -5,6 +5,7 @@ import { Hero } from './hero/Hero'
 import { PopularDestinations } from './popular-destinations/PopularDestinations'
 import { Offers } from './offers/Offers'
 import { WhyUs } from './whyus/WhyUs'
+import { Articles } from './articles/Articles'
 
 
 export const HomeScreen = () => {
@@ -15,6 +16,7 @@ export const HomeScreen = () => {
             <PopularDestinations />
             <Offers />
             <WhyUs />
+            <Articles />
             <Footer />
         </>
     )
---
+++ src/components/screens/home/articles/Articles.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Article } from '../../../shared/card-article/Article'
+import './styles.css'
+
+const array = [1, 2, 3]
+
+export const Articles = () => {
+    return (
+        <section className='articles-section'>
+
+            <div className=" main_container">
+
+                <div className='postTitle-area'>
+                    <div className="titles_container">
+                        <span className="section-tagline left-text">From the blog</span>
+                        <h2 className="section-title left-text">Latest Blog Articles</h2>
+                    </div>
+                    <button name='all posts' className='btn-viewAll'>
+                        <Link to='/'>VIEW ALL POSTS</Link>
+                    </button>
+                </div>
+                
+
+                <div className='cardsOffers_container'>
+                    {
+                        array.map(i => (
+                            <Article />
+                        ))
+                    }
+                </div>
+
+            </div >
+
+        </section>
+    )
+}
