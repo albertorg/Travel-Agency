49a0ec54484b9ebcafd20b5e481d1ecdde07d08a
Alberto Rodriguez <ticoc911@gmail.com>
2023-03-06 21:03:08 +0100

    making structure of hero details

--- src/components/screens/hotel-details/hero/HeroDetails.jsx
+++ src/components/screens/hotel-details/hero/HeroDetails.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import './styles.css'
+import { Slider } from '../../../shared/slider/Slider'
+import { FaMapMarkerAlt } from 'react-icons/fa'
 
 
 export const HeroDetails = ({ hotel }) => {
 
-    
+
 
     return (
         <div className='heroDetails-container'>
             <div className="heroTitle-container">
                 <h1>{hotel.name}</h1>
+                <div className='hero-info'>
+                    <div className="rating-hero"></div>
+                    <div className="address-hero">
+                        <FaMapMarkerAlt />
+                        <span>{hotel.details.address.content}</span>
+                        <span>, {hotel.zoneName}</span>
+                        <span>, {hotel.destinationName}</span>
+                    </div>
+                </div>
             </div>
 
-            <div className="heroImages-container"></div>
+            <div className="heroImages-container">
+                <Slider slides={hotel.details.images}/>
+            </div>
         </div>
     )
 }
