9b9096bf85a36328505c1664784c43e7efa7f457
Alberto Rodriguez <ticoc911@gmail.com>
2023-03-21 06:28:39 +0100

    more styles

--- src/components/screens/hotel-details/rooms/Rooms.jsx
+++ src/components/screens/hotel-details/rooms/Rooms.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Radio, RadioGroup, useRadioState } from "ariakit/radio";
 import { SiAdguard } from 'react-icons/si'
 // import { IoMdClose } from 'react-icons/io'
@@ -8,13 +8,11 @@ import './styles.css'
 
 export const Rooms = ({ rooms }) => {
 
-    const radio = useRadioState( { defaultActiveId: null} )
-    const [style, setStyle] = useState({})
+    const radio = useRadioState({ defaultActiveId: null })
     console.log(radio)
 
     const handleClick = (id) => {
         radio.move(id)
-        setStyle({ borderColor: '#e6eff9' })
     }
 
     return (
@@ -22,10 +20,10 @@ export const Rooms = ({ rooms }) => {
             <RadioGroup state={radio}>
                 {
                     rooms.map((room, idx) => (
-                        <div 
+                        <div
                             className="room-card"
-                            style={radio.value === idx ? { borderColor: '#e6eff9' } : {}} 
-                            key={idx} 
+                            style={radio.value === idx ? { borderColor: '#e6eff9' } : {}}
+                            key={idx}
                             onClick={() => handleClick(radio.items[idx].id)}
                         >
                             <div className="roomName-container">
@@ -45,16 +43,17 @@ export const Rooms = ({ rooms }) => {
                                     <span>Free cancellation until 24/03/23</span>
                                     <HiOutlineInformationCircle />
                                 </div>
-                                <div className="priceRoom-container">
-                                    <span>{`${parseInt(room.rates[0].net)} €`}</span>
-                                    <span>Total price: 2 nith</span>
-                                </div>
+                            </div>
+
+                            <div className="priceRoom-container">
+                                <span className='priceRoom'>{`${parseInt(room.rates[0].net)} €`}</span>
+                                <span className='totalPrice-info'>Total price: 2 nights</span>
                             </div>
                         </div>
                     ))
-                }  
+                }
             </RadioGroup>
-            
+
         </section>
     )
 }
