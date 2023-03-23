3cd8d7c4e715464d48acb3c73402885ace563f61
Alberto Rodriguez <ticoc911@gmail.com>
2023-03-23 08:36:19 +0100

    animation to bar to pay

--- src/components/screens/hotel-details/bar-goToPay/GoToPay.jsx
+++ src/components/screens/hotel-details/bar-goToPay/GoToPay.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import './styles.css'
+import React, { useEffect, useState } from 'react'
 import { formatString } from '../../../../helpers/formatString'
 import { useNavigate } from 'react-router-dom'
+import './styles.css'
 
 export const GoToPay = ({ room, nights }) => {
 
+  const [show, setShow] = useState('')
   const navigate = useNavigate()
   const rate = room.rates[0]
 
@@ -12,8 +13,12 @@ export const GoToPay = ({ room, nights }) => {
     navigate('/pay')
   }
 
+  useEffect(() => {
+    setShow('show')
+  }, [])
+
   return (
-    <div className='goToPay-container'>
+    <div className={`goToPay-container ${show}`} >
 
       <div className='main_container mainSpace'>
         <div className="barInfo-container">
@@ -31,4 +36,5 @@ export const GoToPay = ({ room, nights }) => {
       
     </div>
   )
+
 }
