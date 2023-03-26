6ecae302ee912de3999f133ee7119d5a0b0aad7e
Alberto Rodriguez <ticoc911@gmail.com>
2023-03-26 13:40:17 +0200

    link to privacy done in section privacy

--- src/components/screens/pay/privacy/Privacy.jsx
+++ src/components/screens/pay/privacy/Privacy.jsx
@@ -3,6 +3,22 @@ import './styles.css'
 
 export const Privacy = () => {
   return (
-    <div>Privacy</div>
+    <section className='privacy-section'>
+      <div className="acceptConditions-container styleCondition-container">
+        
+      </div>
+
+      <div className="linkConditions-container styleCondition-container">
+
+        By clicking on "Book now for €{92.59}" you 
+        proceed with payment and confirm that you have read and agree
+        to the <button>Terms and Conditions</button> and the hotel's terms 
+        and conditions regarding changes and cancellations, as well as 
+        read the <button>Privacy Policy</button>. If you would like to opt out of receiving 
+        our email newsletter regarding products and services similar to those you 
+        are purchasing please write to support@funtravels.com
+        
+      </div>
+    </section>
   )
 }
