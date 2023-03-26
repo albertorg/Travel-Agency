d4a60943d796b8a4c49238959751ae4e1df61710
Alberto Rodriguez <ticoc911@gmail.com>
2023-03-26 15:35:25 +0200

    privacy section done

--- src/components/screens/pay/privacy/Privacy.jsx
+++ src/components/screens/pay/privacy/Privacy.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Checkbox } from '@mui/material';
 import './styles.css'
 
 export const Privacy = () => {
+
+  const label = 'You consent, as per point 3. F. of the Privacy Policy, to receive personalized business proposals by mail, email, text message, telephone, or automatically processed online advertisements, including proposals and offers on products and services from us or selected third parties (with whom we do not share your information collected here). To revoke your consent, write to privacy.it@lastminutegroup.com. Show less'
+
   return (
     <section className='privacy-section'>
       <div className="acceptConditions-container styleCondition-container">
+        <Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }} />
         
+        <div className='label-check'>
+          <span>Send me travel offers</span>
+          <span>You consent, as per point 3. F. of the <button>Privacy Policy</button>.</span>
+        </div>
       </div>
 
       <div className="linkConditions-container styleCondition-container">
