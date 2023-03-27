54b8751f33d540d30efce53c9f71f427bb8d4d9e
Alberto Rodriguez <ticoc911@gmail.com>
2023-03-27 13:59:30 +0200

    structure of aside bar

--- src/components/screens/pay/sideBar/SideBar.jsx
+++ src/components/screens/pay/sideBar/SideBar.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import { BsFillKeyFill } from 'react-icons/bs'
+import { HiStar } from 'react-icons/hi'
+import shield_stars from '../../../../assets/payment/shield_stars_vg.svg'
 import './styles.css'
 
 export const SideBar = () => {
   return (
-    <aside>SideBar</aside>
+    <aside>
+      <div className='aside-title'>
+        <h3>Your trip</h3>
+      </div>
+
+      <div className='info-trip-container'>
+        <div className='data-hotel-container show-edge'>
+          <div className='key-round'>
+            <BsFillKeyFill />
+          </div>
+
+          <div>
+            <div className='name-ctegory'>
+              <span>Hotel Brisas Guardalavaca</span>
+              <HiStar />
+            </div>
+
+            <div>Playa Guardalavaca Banes, Playa Guardalavaca</div>
+            <div>mer 29 mar 2023gio 30 mar 2023</div>
+
+            <div>
+              <div>1X</div>
+              <div>
+                <span>1 Notte, All inclusive</span>
+                <span>2 Adulti</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div className='free-cancel-container show-edge'>
+          <div>
+            <h4>Free cancellation</h4>
+            <span>
+              You can cancel the reservation of your
+              your room until March 29, 2023.
+            </span>
+          </div>
+          <div>
+            <img src={shield_stars} alt="shield_stars" />
+          </div>
+        </div>
+
+        <div className='total-price-container'>
+          <span>Total price</span>
+          <span>92,87 €</span>
+        </div>
+
+      </div>
+    </aside>
   )
 }
