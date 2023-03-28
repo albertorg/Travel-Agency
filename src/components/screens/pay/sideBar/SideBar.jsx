a8a35d19387960ce5cf7c15718e6346332358767
Alberto Rodriguez <ticoc911@gmail.com>
2023-03-28 05:17:54 +0200

    some styles to aside screen pay

--- src/components/screens/pay/sideBar/SideBar.jsx
+++ src/components/screens/pay/sideBar/SideBar.jsx
@@ -6,54 +6,56 @@ import './styles.css'
 
 export const SideBar = () => {
   return (
-    <aside>
-      <div className='aside-title'>
-        <h3>Your trip</h3>
-      </div>
+    <aside >
+      <div className='aside-hotel-info'>
+        <div className='aside-title'>
+          <h3>Your trip</h3>
+        </div>
 
-      <div className='info-trip-container'>
-        <div className='data-hotel-container show-edge'>
-          <div className='key-round'>
-            <BsFillKeyFill />
-          </div>
-
-          <div>
-            <div className='name-ctegory'>
-              <span>Hotel Brisas Guardalavaca</span>
-              <HiStar />
+        <div className='info-trip-container'>
+          <div className='data-hotel-container show-edge'>
+            <div className='key-round'>
+              <BsFillKeyFill />
             </div>
 
-            <div>Playa Guardalavaca Banes, Playa Guardalavaca</div>
-            <div>mer 29 mar 2023gio 30 mar 2023</div>
-
             <div>
-              <div>1X</div>
+              <div className='name-ctegory'>
+                <span>Hotel Brisas Guardalavaca</span>
+                <HiStar />
+              </div>
+
+              <div>Playa Guardalavaca Banes, Playa Guardalavaca</div>
+              <div>mer 29 mar 2023gio 30 mar 2023</div>
+
               <div>
-                <span>1 Notte, All inclusive</span>
-                <span>2 Adulti</span>
+                <div>1X</div>
+                <div>
+                  <span>1 Notte, All inclusive</span>
+                  <span>2 Adulti</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        
-        <div className='free-cancel-container show-edge'>
-          <div>
-            <h4>Free cancellation</h4>
-            <span>
-              You can cancel the reservation of your
-              your room until March 29, 2023.
-            </span>
-          </div>
-          <div>
-            <img src={shield_stars} alt="shield_stars" />
-          </div>
-        </div>
 
-        <div className='total-price-container'>
-          <span>Total price</span>
-          <span>92,87 €</span>
-        </div>
+          <div className='free-cancel-container show-edge'>
+            <div>
+              <h4>Free cancellation</h4>
+              <span>
+                You can cancel the reservation of your
+                your room until March 29, 2023.
+              </span>
+            </div>
+            <div>
+              <img src={shield_stars} alt="shield_stars" />
+            </div>
+          </div>
 
+          <div className='total-price-container'>
+            <span>Total price</span>
+            <span>92,87 €</span>
+          </div>
+
+        </div>
       </div>
     </aside>
   )
