48002a2910144e508b3d7cd119a0d00b0c8bc5c1
Alberto Rodriguez <ticoc911@gmail.com>
2023-03-30 18:06:51 +0200

    some styles added to done screen

--- src/components/screens/pay-done/PayDone.jsx
+++ src/components/screens/pay-done/PayDone.jsx
@@ -10,59 +10,57 @@ export const PayDone = () => {
     <main className='main_container main-Done'>
       <h2>Congratulation 🎉</h2>
       <div className='yourBooking-container'>
+        <h3>Your booking</h3>
 
-        <div className='hotelImg_container'>
-          <img src="http://photos.hotelbeds.com/giata/bigger/00/004200/004200a_hb_ro_006.jpg" alt="hotel" />
-        </div>
-
-        <div className='inf-container' >
-
-          <span className='destination_name'>
-            {`Playa Guardalavaca, Holguin, Cuba`}
-          </span>
-          <h3 className='hotel_name'>
-            {`Brisas Guardalavaca All Inclusive`}
-          </h3>
-          <CategoryStars category={`4`}/>
-          <div className='cancel-info'>
-            <HiBadgeCheck />
-            &nbsp;
-            Free Cancellation Option
-          </div>
-          <div className='all_inclusive'>
-            <GiCoffeeCup />
-            &nbsp;&nbsp;
-            {`All Inclusive`}
+        <div className='yourBooking-main'>
+          <div className='hotelImg_container'>
+            <img src="http://photos.hotelbeds.com/giata/bigger/00/004200/004200a_hb_ro_006.jpg" alt="hotel" />
           </div>
 
+          <div className='inf-container' >
+
+            <span className='destination_name'>
+              {`Playa Guardalavaca, Holguin, Cuba`}
+            </span>
+            <h3 className='hotel_name'>
+              {`Brisas Guardalavaca All Inclusive`}
+            </h3>
+            <CategoryStars category={`4`} />
+            <div className='center-icons'>
+              <HiBadgeCheck />
+              &nbsp;
+              Free Cancellation Option
+            </div>
+            <div className='center-icons'>
+              <GiCoffeeCup />
+              &nbsp;&nbsp;
+              {`All Inclusive`}
+            </div>
+
+          </div>
         </div>
       </div>
 
       <div className='bookingDetails-container'>
         <h3>Booking details</h3>
         <div className='bookingDetails'>
-          <div>
+          <div className='grid-details'>
             <span>Booking code:</span>
             <span>222-333-111</span>
           </div>
-          <div>
+          <div className='grid-details'>
             <span>Date:</span>
             <span>{`12 Aug, 2023`}</span>
           </div>
-          <div>
+          <div className='grid-details'>
             <span>Total:</span>
             <span>{`€  199`}</span>
           </div>
-          <div>
+          <div className='grid-details'>
             <span>Payment method:</span>
             <span>Credit card</span>
           </div>
         </div>
-      </div>
-
-      <div className='exploreMore-container'>
-        <button>Explore more stays</button>
-        <Link to='/'>My bookings</Link>
       </div>
 
       <div className='confirmation-mail'>
@@ -72,6 +70,11 @@ export const PayDone = () => {
           check-in and check-out dates, the type of room you have
           chosen and the confirmation number.
         </span>
+      </div>
+
+      <div className='exploreMore-container'>
+        <button>Explore more stays</button>
+        <Link to='/'>My bookings</Link>
       </div>
 
     </main>
