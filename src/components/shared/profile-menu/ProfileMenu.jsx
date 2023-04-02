970e856ef3e357917b93b4b3c133cb96a908662c
Alberto Rodriguez <ticoc911@gmail.com>
2023-04-02 23:11:43 +0200

    styled menu profile

--- src/components/shared/profile-menu/ProfileMenu.jsx
+++ src/components/shared/profile-menu/ProfileMenu.jsx
@@ -14,7 +14,7 @@ import './styles.css'
 
 export const ProfileMenu = () => {
 
-    const menu = useMenuState({ gutter: 8, placement: 'bottom-end' });
+    const menu = useMenuState({ gutter: 23, placement: 'bottom-end' });
     console.log(menu)
 
     return (
@@ -31,7 +31,7 @@ export const ProfileMenu = () => {
                     <HiOutlineClipboardList/>
                     My bookings
                 </MenuItem>
-                <MenuItem className="menu-item" disabled>
+                <MenuItem className="menu-item">
                     <SlSettings />
                     Settings
                 </MenuItem>
